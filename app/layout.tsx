4d7e2f23c3a68071d6698936f7c5c95658552c76
Malekqandeel <malikqan@gmail.com>
2025-12-02 22:10:57 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
-// app/layout.jsx
+// app/layout.tsx
 import "./globals.css";
 import LanguageProvider from "@/components/LanguageProvider";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "JobReady",
   description: "Job readiness program",
 };
 
-export default function RootLayout({ children }) {
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body>
